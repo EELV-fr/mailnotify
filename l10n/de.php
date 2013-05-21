fa2c51ccf5e56e52f3ce54cc5b1650e9be5f0333
telcy <jascha.burmeister@gmail.com>
2013-05-21 14:57:14 +0200

    fixed german translation

--- l10n/de.php
+++ l10n/de.php
@@ -2,6 +2,6 @@
 	'Notify' => 'Notify',
 	'There was'=>'Es wurden',
 	'new files uploaded in'=>'neue Dateien hochgeladen',
-	'New upload'=>'New upload',
-	'This e-mail is automatic, please, do not reply to it. If you no longer want to receive theses alerts, disable notification on each shared items.'=>'This e-mail is automatic, please, do not reply to it. If you no longer want to receive theses alerts, disable notification on each shared items.',
+	'New upload'=>'Neuer Upload',
+	'This e-mail is automatic, please, do not reply to it. If you no longer want to receive theses alerts, disable notification on each shared items.'=>'Dies ist eine automatische Mail. Bitte antworten Sie nicht darauf. Wenn Sie nicht länger diese Benachrichtigung erhalten wollen, dann deaktivieren Sie die Option bei den geteilten Ordnern.',
 );
