1f68babdecab31607b399da4d6357188209b880c
telcy <jascha.burmeister@gmail.com>
2013-05-22 11:07:34 +0200

    added de*translation for new message

--- l10n/de.php
+++ l10n/de.php
@@ -4,4 +4,5 @@
 	'new files uploaded in'=>'neue Dateien hochgeladen',
 	'New upload'=>'Neuer Upload',
 	'This e-mail is automatic, please, do not reply to it. If you no longer want to receive theses alerts, disable notification on each shared items.'=>'Dies ist eine automatische Mail. Bitte antworten Sie nicht darauf. Wenn Sie nicht länger diese Benachrichtigung erhalten wollen, dann deaktivieren Sie die Option bei den geteilten Ordnern.',
+	'You have a new message from %s'=>'Du hast eine neue Nachricht von %s',
 );
