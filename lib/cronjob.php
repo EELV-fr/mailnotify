50ca2e1f932e8072e05e6ba87e497deaa448f32c
border0464111 <fredericguilbault@live.ca>
2013-09-15 08:34:40 -0400

    Update cronjob.php

--- lib/cronjob.php
+++ lib/cronjob.php
@@ -6,9 +6,27 @@
 
 	include_once $cloud_root.'/lib/base.php';
 	include 'mailing.php';
-
 	OC_MailNotify_Mailing::do_notification_queue();
-
 	
+//
 
-?>
+//TODO owncloud's cron jobs methode
+/*
+static OC_BackgroundJob_QueuedTask::add	(	 	$app,
+ 	$klass,
+ 	$method,
+ 	$parameters 
+)		
+static
+queues a task
+
+Parameters
+$app	app name
+$klass	class name
+$method	method name
+$parameters	all useful data as text
+Returns
+id of task
+ * 
+ * 
+ */
